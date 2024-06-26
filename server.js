6dcfe0e7de651296e7915b490db353f947e7382f
KhadijaCap4 <khadija.sy@cap4lab.com>
2024-06-26 18:57:45 +0200

    creation of the digital media site and backend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware pour servir des fichiers statiques
+app.use(express.static('public'));
+
+// Route de base
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
